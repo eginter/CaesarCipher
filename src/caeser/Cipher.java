062fd37248cfdd5ace0bdf53826cf3a5fe5a9f0f
Elijah Ginter <ginterelijah@gmail.com>
2016-07-25 17:09:43 -0600

    Added CipherTest + basic logic

--- src/caeser/Cipher.java
+++ src/caeser/Cipher.java
@@ -1,8 +1,27 @@
 package caeser;
 
 public class Cipher {
-	public static void main(String[] args) {
-		System.out.println("Hello World.");
+	char input;
+	int shift;
+	
+	public void encode() {
+		int unicode = input;
+
+//		if ( ! Character.isLetter(input) )
+//			return input;
+
+		unicode = unicode + shift;
+		if ( Character.isUpperCase(input)   && unicode > 'Z' 
+			|| Character.isLowerCase(input) && unicode > 'z' ) {
+			unicode -= 26;
+		}
+		if ( Character.isUpperCase(input)  && unicode < 'A' 
+			|| Character.isLowerCase(input) && unicode < 'a' ) {
+			unicode += 26;
+		}
 	}
 
+	public void decode() {
+		
+	}
 }
---
+++ src/caeser/CipherTest.java
@@ -0,0 +1,7 @@
+package caeser;
+
+public class CipherTest {
+	public static void main(String[] args) {
+		
+	}
+}
