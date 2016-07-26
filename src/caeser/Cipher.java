947034f9431ace09ce23e59a40de919827fb3bc2
Elijah Ginter <ginterelijah@gmail.com>
2016-07-26 08:32:17 -0600

    Fixed issue with converting punctuations.

--- src/caeser/Cipher.java
+++ src/caeser/Cipher.java
@@ -9,8 +9,8 @@ public class Cipher {
 	}
 
 	public void setInput(String input) {
-		phrase = input.toCharArray();
-		this.phrase = phrase;
+		this.phrase = input.toCharArray();
+
 	}
 
 	public int getShift() {
@@ -22,28 +22,35 @@ public class Cipher {
 	}
 
 	public void encode() {
+		String actual_val;
 		for (char input : phrase) {
 			int unicode = input;
-
-//			 if ( ! Character.isLetter(input) )
-//			 return ;
-
 			unicode = unicode + shift;
-			if (Character.isUpperCase(input) && unicode > 'Z' || Character.isLowerCase(input) && unicode > 'z') {
-				unicode -= 26;
+			if (!Character.isLetter(input)) {
+				System.out.print(input);
+			} else {
+				if (Character.isUpperCase(input) && unicode > 'Z' || Character.isLowerCase(input) && unicode > 'z') {
+					unicode -= 26;
+				}
+				if (Character.isUpperCase(input) && unicode < 'A' || Character.isLowerCase(input) && unicode < 'a') {
+					unicode += 26;
+				}
+				if (input == 32) { // if input equals unicode 32 ['\u0020']
+									// which is whitespace,
+					unicode = 32; // unicode == 32, or whitespace
+				}
+				// if (Character.isUpperCase(input) && unicode < 'A' ||
+				// Character.isLowerCase(input) && unicode > 'Z' && unicode <
+				// 'a' ||
+				// Character.isLowerCase(input) && unicode > 'z' ) {
+				// System.out.println("Sorry! Only letters are allowed to be
+				// entered
+				// into the Cipher! ");
+				// break;
+				// } // this didn't work :( Tried to handle punctuation
+				actual_val = Character.toString((char) unicode);
+				System.out.print(actual_val);
 			}
-			if (Character.isUpperCase(input) && unicode < 'A' || Character.isLowerCase(input) && unicode < 'a') {
-				unicode += 26;
-			}
-			if (input == 32) { // if input equals unicode 32 ['\u0020'] which is whitespace,
-				unicode = 32 ; // unicode == 32, or whitespace
-			}
-//			if (Character.isUpperCase(input) && unicode < 'A' || Character.isLowerCase(input) && unicode > 'Z' && unicode < 'a' || Character.isLowerCase(input) && unicode > 'z' ) {
-//				System.out.println("Sorry! Only letters are allowed to be entered into the Cipher! ");
-//				break;
-//			}                                                             // this didn't work :( Tried to handle punctuation
-			String actual_val = Character.toString((char) unicode);
-			System.out.print(actual_val);
 		}
 	}
 
