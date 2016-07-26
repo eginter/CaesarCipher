686bdc7f146457a72a94a526c09b295608fdda0a
Levi Gaston (Pat G) <Pat-Levi-Gaston@Patricks-MacBook-Pro.local>
2016-07-25 18:22:36 -0600

    Added decode method to Cipher class

--- src/caeser/Cipher.java
+++ src/caeser/Cipher.java
@@ -1,27 +1,47 @@
 package caeser;
 
 public class Cipher {
-	char input;
-	int shift;
-	
+	private char[] phrase;
+	private int shift;
+
+	public char[] getInput() {
+		return phrase;
+	}
+
+	public void setInput(String input) {
+		phrase = input.toCharArray();
+		this.phrase = phrase;
+	}
+
+	public int getShift() {
+		return shift;
+	}
+
+	public void setShift(int shift) {
+		this.shift = shift;
+	}
+
 	public void encode() {
-		int unicode = input;
+		for (char input : phrase) {
+			int unicode = input;
 
-//		if ( ! Character.isLetter(input) )
-//			return input;
+			// if ( ! Character.isLetter(input) )
+			// return input;
 
-		unicode = unicode + shift;
-		if ( Character.isUpperCase(input)   && unicode > 'Z' 
-			|| Character.isLowerCase(input) && unicode > 'z' ) {
-			unicode -= 26;
-		}
-		if ( Character.isUpperCase(input)  && unicode < 'A' 
-			|| Character.isLowerCase(input) && unicode < 'a' ) {
-			unicode += 26;
+			unicode = unicode + shift;
+			if (Character.isUpperCase(input) && unicode > 'Z' || Character.isLowerCase(input) && unicode > 'z') {
+				unicode -= 26;
+			}
+			if (Character.isUpperCase(input) && unicode < 'A' || Character.isLowerCase(input) && unicode < 'a') {
+				unicode += 26;
+			}
+			String actual_val = Character.toString((char) unicode);
+			System.out.print(actual_val);
 		}
 	}
 
 	public void decode() {
-		
+		shift = shift * -1;
+		encode();
 	}
 }
--- src/caeser/CipherTest.java
+++ src/caeser/CipherTest.java
@@ -1,7 +1,34 @@
 package caeser;
+import java.util.Scanner;
 
 public class CipherTest {
 	public static void main(String[] args) {
+		Cipher c1 = new Cipher();
+		Scanner keyboard = new Scanner(System.in);
+		int decision;
+		String word;
+		int shiftamount;
 		
+		System.out.println("Would you like to encode a message or decode?");
+		System.out.print("Enter 1 for encode and 2 for decode: ");
+		decision = keyboard.nextInt();
+		
+		System.out.print("Enter the word: ");
+		keyboard.nextLine();
+		word = keyboard.nextLine();
+		
+		System.out.print("Enter the shift amount (i.e. 1):");
+		shiftamount = keyboard.nextInt();
+		
+		if (decision == 1) {
+			c1.setInput(word);
+			c1.setShift(shiftamount);
+			c1.encode();
+		}
+		else {
+			c1.setInput(word);
+			c1.setShift(shiftamount);
+			c1.decode();
+		}
 	}
 }
